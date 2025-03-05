0a463a799fdffc739a557eef40b2fb9ea7c77142
excel007 <eak.ekkachai@gmail.com>
2025-03-05 13:25:26 +0700

    Add error handling for missing DATABASE_URL and clean up imports in schema and page components

--- app/lib/db/db.ts
+++ app/lib/db/db.ts
@@ -4,6 +4,9 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not defined')
+}
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false })
